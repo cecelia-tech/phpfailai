43c2aed532b288d3284f454e87bb915caeab00c5
cecelia-tech <79083749+cecelia-tech@users.noreply.github.com>
2021-05-29 00:52:48 +0300

    9 tasks done

--- namuDarbai/kintamiejiIrSalygos.php
+++ namuDarbai/kintamiejiIrSalygos.php
@@ -15,33 +15,33 @@ echo "<br>";
 echo 'Antra uzduotis'. "<br>";
 echo "<br>";
 
-$skaicius1 = rand(0, 4);
-$skaicius2 = rand(0, 4);
-echo "<br>";
-echo $skaicius1 . "<br>";
-echo $skaicius2 . "<br>";
-$dalmuo = 0;
-if ($skaicius1 > $skaicius2) {
-    $dalmuo = $skaicius1 / $skaicius2;
-} elseif ($skaicius2 > $skaicius1) {
-    $dalmuo = $skaicius2 / $skaicius1;
-} elseif ($skaicius1 = $skaicius2) {
-    echo 'Skaiciai yra lygus'. "<br>";
-    $dalmuo = $skaicius1 / $skaicius2;
-} elseif ($skaicius1 == 0 or $skaicius2 == 0 ) {
-    echo 'Dalyba is 0 negalima';
-}else {
-    echo 'Vienas arba abu skaiciai yra 0';
-}
+// $skaicius1 = rand(0, 4);
+// $skaicius2 = rand(0, 4);
+// echo "<br>";
+// echo $skaicius1 . "<br>";
+// echo $skaicius2 . "<br>";
+// $dalmuo = 0;
+// if ($skaicius1 > $skaicius2) {
+//     $dalmuo = $skaicius1 / $skaicius2;
+// } elseif ($skaicius2 > $skaicius1) {
+//     $dalmuo = $skaicius2 / $skaicius1;
+// } elseif ($skaicius1 = $skaicius2) {
+//     echo 'Skaiciai yra lygus'. "<br>";
+//     $dalmuo = $skaicius1 / $skaicius2;
+// } elseif ($skaicius1 == 0 or $skaicius2 == 0 ) {
+//     echo 'Dalyba is 0 negalima';
+// }else {
+//     echo 'Vienas arba abu skaiciai yra 0';
+// }
 
 
 
-echo "<br>";
-echo $skaicius1 . "<br>";
-echo $skaicius2 . "<br>";
-echo 'Atsakymas' . "<br>";
-echo (round($dalmuo, 2));
-echo "<br>";
+// echo "<br>";
+// echo $skaicius1 . "<br>";
+// echo $skaicius2 . "<br>";
+// echo 'Atsakymas' . "<br>";
+// echo (round($dalmuo, 2));
+// echo "<br>";
 
 echo 'Trecia uzduotis'. "<br>";
 echo "<br>";
@@ -64,11 +64,75 @@ if (($sk2 > $sk1 && $sk2 < $sk3) || ($sk2 < $sk1 && $sk2 > $sk3)) {
 } else {
     echo "Vidurinis skaicius yra $sk3" . "<br>";
 }
+echo 'Ketvirta uzduotis'. "<br>";
+echo "<br>";
 
+$krastine1 = rand(1,10);
+$krastine2 = rand(1,10);
+$krastine3 = rand(1,10);
 
-// elseif ($sk3 > $sk1 && $sk3 < $sk2) {
-//     echo $sk3 . "<br>";
-// } 
+if (($krastine1 + $krastine2 > $krastine3) 
+    || ($krastine1 + $krastine3 > $krastine2)
+    || ($krastine2 + $krastine3 > $krastine1)
+) {
+    echo 'Trikampi sudaryti galima';
+} else {
+    echo 'Trikampis nesusidaro';
+}
+echo "<br>";
+echo 'Penkta uzduotis'. "<br>";
+echo "<br>";
+
+$kintamasis1 = rand(0, 2);
+$kintamasis2 = rand(0, 2);
+$kintamasis3 = rand(0, 2);
+$kintamasis4 = rand(0, 2);
+
+$nulisCount = 0;
+$vienasCount = 0;
+$duCount = 0;
+
+if ($kintamasis1 == 0){
+    $nulisCount++;
+} 
+if ($kintamasis1 == 1) {
+    $vienasCount++;
+}
+if ($kintamasis1 == 2) {
+    $duCount++;
+}
+if ($kintamasis2 === 0) {
+    $nulisCount++;
+}
+if ($kintamasis2 === 1) {
+    $vienasCount++;
+}
+if ($kintamasis2 === 2) {
+    $duCount++;
+}
+if ($kintamasis3 === 0) {
+    $nulisCount++;
+}
+if ($kintamasis3 === 1) {
+    $vienasCount++;
+}
+if ($kintamasis3 === 2) {
+    $duCount++;
+}
+if ($kintamasis4 === 0) {
+    $nulisCount++;
+}
+if ($kintamasis4 === 1) {
+    $vienasCount++;
+}
+if ($kintamasis4 === 2) {
+    $duCount++;
+}
+
+echo $nulisCount . "<br>";
+echo $vienasCount. "<br>";
+echo $duCount. "<br>";
+
 echo "<br>";
 echo 'Sesta uzduotis'. "<br>";
 
@@ -77,3 +141,116 @@ $atsitiktinisSkaicius = rand(1, 6);
 echo "<h$atsitiktinisSkaicius>$atsitiktinisSkaicius</h$atsitiktinisSkaicius>";
 
 echo 'Septinta uzduotis'. "<br>";
+$skaicius1 = rand(-10, 10);
+$skaicius2 = rand(-10, 10);
+$skaicius3 = rand(-10, 10);
+
+if ($skaicius1 < 0) {
+    echo "<div style='color: green;'>$skaicius1</div> ";
+}
+if ($skaicius1 === 0) {
+    echo "<div style ='color: red;'>$skaicius1</div> ";
+}
+if ($skaicius1 > 0) {
+    echo "<div style ='color: blue;'>$skaicius1</div> ";
+}
+if ($skaicius2 < 0) {
+    echo "<div style='color: green;'>$skaicius2</div> ";
+}
+if ($skaicius2 === 0) {
+    echo "<div style ='color: red;'>$skaicius2</div> ";
+}
+if ($skaicius2 > 0) {
+    echo "<div style ='color: blue;'>$skaicius2</div> ";
+}
+if ($skaicius3 < 0) {
+    echo "<div style='color: green;'>$skaicius3</div> ";
+}
+if ($skaicius3 === 0) {
+    echo "<div style ='color: red;'>$skaicius3</div> ";
+}
+if ($skaicius3 > 0) {
+    echo "<div style ='color: blue;'>$skaicius3</div> ";
+}
+
+echo 'Astunta uzduotis'. "<br>";
+
+$zvakiuKiekis = rand(5, 3000);
+$zvakesKaina = 1;
+$moketinaSuma = $zvakesKaina * $zvakiuKiekis;
+
+if ($moketinaSuma >= 1000 && $moketinaSuma < 2000) {
+    $zvakesKaina = 1 * 0.97;
+    //$moketinaSuma = $zvakiuKiekis * $zvakesKaina;
+    
+} elseif ($moketinaSuma >= 2000) {
+    $zvakesKaina = 1 * 0.96;
+    //$moketinaSuma = $zvakiuKiekis * $zvakesKaina;
+} 
+
+echo $zvakiuKiekis. "<br>";
+echo $zvakesKaina. "<br>";
+
+echo 'Devinta uzduotis'. "<br>";
+echo "<br>";
+
+$skaicius1 = rand(0, 100);
+$skaicius2 = rand(0, 100);
+$skaicius3 = rand(0, 100);
+
+echo $skaicius1 . "<br>";
+echo $skaicius2 . "<br>";
+echo $skaicius3 . "<br>";
+//3 yra skaiciu kiekis
+echo round(($skaicius1 + $skaicius2 + $skaicius3) / 3, 0);
+
+$count = 0;
+
+if ($skaicius1 < 10) {
+    # code...
+}
+
+echo "<br>";
+echo 'Desimta uzduotis'. "<br>";
+echo "<br>";
+
+$valandos = rand(0, 23);
+$minutes = rand(0, 59);
+$sekundes = rand(0, 59);
+$papildomosSekundes = rand(0, 300);
+
+echo $valandos . ':' . $minutes . ':' . $sekundes;
+echo "<br>";
+echo $papildomosSekundes;
+echo "<br>";
+
+if ($papildomosSekundes % 60 > 0) {
+   $liekana = $papildomosSekundes % 60;
+   $papildomosMinutes = ($papildomosSekundes - ($liekana)) / 60;
+   if ($sekundes + $liekana > 60) {
+       $minutes++;
+       $sekundes = ($sekundes + $liekana) - 60;
+   } else {
+       $sekundes += $liekana;
+   }
+   if ($minutes + $papildomosMinutes > 60) {
+       $valandos++;
+       $minutes = ($minutes + $papildomosMinutes) - 60;
+   } else {
+       $minutes += $papildomosMinutes;
+   }
+   if ($valandos > 24) {
+       $valandos = 00;
+   }
+} else {
+    if ($minutes + ($papildomosSekundes / 60) > 60) {
+        $valandos++;
+        $minutes = ($minutes + $papildomosMinutes) - 60;
+    } else {
+        $minutes += ($papildomosSekundes / 60);
+    }
+    if ($valandos >24) {
+       $valandos = 00;
+   }
+}
+echo $valandos . ':' . $minutes . ':' . $sekundes;
