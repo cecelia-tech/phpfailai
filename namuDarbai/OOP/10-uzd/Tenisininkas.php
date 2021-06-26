7c1bcdf6417863a75b2bb26af4cffc4f71532605
cecelia-tech <79083749+cecelia-tech@users.noreply.github.com>
2021-06-26 17:17:07 +0300

    10th attempt

--- namuDarbai/OOP/10-uzd/Tenisininkas.php
+++ namuDarbai/OOP/10-uzd/Tenisininkas.php
@@ -7,5 +7,53 @@ class Tenisininkas {
     private static $zaidejas1;
     private static $zaidejas2;
 
+    public function __construct($vardas)
+    {
+        $this->vardas = $vardas;
+        $this->kamuoliukas = false;
+    }
+    public static function getZaideja($name)
+    {
+        if (!(isset(self::$zaidejas1))) {
+            return self::$zaidejas1 = new self($name);
+        } elseif (!(isset(self::$zaidejas2))) {
+            return self::$zaidejas2 = new self($name);
+        } else {
+            echo 'Tik 2 zaidejai';
+        }
+    }
+    public function arTuriKamuoliuka(){
+        return $this->kamuoliukas;
+    }
 
+    public function perduotiKamuoliuka()
+    {
+
+    }
+    public function zaidimoPradzia()
+    {
+
+    }
+    // public static function __set($prop, $value)
+    // {
+    //     // echo 'atejo';
+    //     // die;
+    //     // if ($prop == 'zaidejas1') {
+    //     //     self::$zaidejas1 = $value;
+    //     // }
+    //         return self::$zaidejas1 = $value;
+
+    // }
+    // public static function __get($prop)
+    // {
+    //     return self::$zaidejas1;
+    // }
+    // public function ReflectionClass::setStaticPropertyValue()
+    // {
+    //     echo 'atejo';
+    //     die;
+    //     if (true) {
+    //         self::$zaidejas1 = $value;
+    //     }
+    // }
 }
--- namuDarbai/OOP/10-uzd/index.php
+++ namuDarbai/OOP/10-uzd/index.php
@@ -1,2 +1,14 @@
 <?php
 
+include __DIR__. '/Tenisininkas.php';
+$zaidejas = Tenisininkas::getZaideja('Rob');
+$zaidejas2 = Tenisininkas::getZaideja('Bob');
+//$zaidejas3 = Tenisininkas::getZaideja('John');
+//Tenisininkas::$zaidejas1 = $zaidejas;
+//Tenisininkas::set(new Tenisininkas('Rob'));
+echo "<pre>";
+//var_dump(Tenisininkas::$zaidejas1);
+var_dump($zaidejas);
+var_dump($zaidejas2);
+echo "<br>";
+echo $zaidejas->arTuriKamuoliuka();
